f20965a9c40712217f30dd7dec0a859613073abe
Th3Vladimir <vladimir.vor@seznam.cz>
2023-10-20 22:53:51 +0200

    feat: add flower class and scripts

---
+++ Assets/Scripts/Flower/Flower.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FlowerType
+{
+    Main,
+    Attack,
+    Defense,
+    Economic
+}
+
+public interface IFlower
+{
+    FlowerType Type { get; }
+    int Health { get; set; }
+    float Radius { get; set; }
+    void TakeDamage(int amount);
+
+}
+
+
+public class Flower : IFlower
+{
+    public FlowerType Type { get; private set; }
+    public int Health { get; set; }
+    public float Radius { get; set; }
+
+
+    public Flower(FlowerType type, float radius)
+    {
+        this.Type = type;
+        this.Health = 100;
+        this.Radius = radius;
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+        this.Health -= amount;
+
+        if (this.Health <= 0)
+        {
+            DestroyFlower();
+        }
+    }
+
+
+    private void DestroyFlower()
+    {
+
+    }
+
+}
+
+public class FlowerMain : Flower
+{
+
+    public FlowerMain(float radius) : base(FlowerType.Main, radius)
+    {
+
+    }
+
+    public void ShootOutBud(string budType, Vector3 position)
+    {
+
+    }
+}
+
+
---
+++ Assets/Scripts/Flower/MainFlowerScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainFlowerScript : MonoBehaviour
+{
+    [SerializeField] float radius = 20f;
+    [SerializeField] FlowerType type;
+
+    public Flower flower;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flower = new Flower(type, radius);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check for collision
+        //  player - water
+        //  
+
+
+    }
+}
