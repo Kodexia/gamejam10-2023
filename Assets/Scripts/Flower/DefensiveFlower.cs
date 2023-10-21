24065d2bb20c21cfac0e2313010bca7279af1028
Fami6Xx <libojer@gmail.com>
2023-10-21 11:47:36 +0200

    feat: defensive flower script

---
+++ Assets/Scripts/Flower/DefensiveFlower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefensiveFlower : MonoBehaviour
+{
+    [SerializeField]
+    private float radius = 20f;
+    
+    [SerializeField]
+    private GameObject flowerObject;
+    
+    [SerializeField]
+    private FlowerType type;
+    
+    [SerializeField]
+    private float maxHealth = 600f;
+    
+    public Flower Flower { get; private set; }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Flower = new Flower(flowerObject, FlowerType.Defense, radius, maxHealth);
+        Flower.Priority = 3;
+    }
+}
