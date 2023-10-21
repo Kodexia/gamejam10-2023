5b5665b7e406b25b412265cc043ee24b6e77e1a8
Fami6Xx <libojer@gmail.com>
2023-10-22 00:08:46 +0200

    feat: commented free spawning of buds

--- Assets/Scripts/Flower/MainFlowerScript.cs
+++ Assets/Scripts/Flower/MainFlowerScript.cs
@@ -51,7 +51,7 @@ public class MainFlowerScript : MonoBehaviour
     {
         if (other.gameObject.CompareTag(GameManager.instance.playerTag))
         {
-            BudSpawnerManager.instance.SpawnNewFlowerBud(flowerBudPrefabDefensive);
+            //BudSpawnerManager.instance.SpawnNewFlowerBud(flowerBudPrefabDefensive);
             //EnemySpawnerManager.instance.ChangeSpawnRate(0.001f);
 
             //Debug.Log("Player has collided with the flower!");
