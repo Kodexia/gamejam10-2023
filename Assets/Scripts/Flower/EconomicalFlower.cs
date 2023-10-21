b54a8f9849d012664812952cdf49d04d1cb186fb
Th3Vladimir <vladimir.vor@seznam.cz>
2023-10-21 11:50:57 +0200

    feat: added scene

--- Assets/Scripts/Flower/EconomicalFlower.cs
+++ Assets/Scripts/Flower/EconomicalFlower.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EconomicalFlower : MonoBehaviour
+public class EconomicalFlower : FlowerScript
 {
     [SerializeField]
     private float radius = 20f;
