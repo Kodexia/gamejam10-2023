3a02743b067119988c1a6dd9d54d2babfe589f1a
AdamMakoun <adammakaeu@gmail.com>
2023-10-21 16:04:13 +0200

    added grass to defense flower

--- Assets/Scripts/Flower/EconomicalFlower.cs
+++ Assets/Scripts/Flower/EconomicalFlower.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EconomicalFlower : MonoBehaviour
+public class EconomicalFlower : FlowerScript
 {
     [SerializeField]
     private float boostSpeed = 2f;
--- Assets/Scripts/Flower/MainFlowerScript.cs
+++ Assets/Scripts/Flower/MainFlowerScript.cs
@@ -39,7 +39,7 @@ public class MainFlowerScript : MonoBehaviour
     {
         if (other.gameObject.CompareTag(GameManager.instance.playerTag))
         {
-            SpawnerManager.instance.SpawnNewFlowerBud(flowerBudPrefabEconomic);
+            SpawnerManager.instance.SpawnNewFlowerBud(flowerBudPrefabDefensive);
 
             Debug.Log("Player has collided with the flower!");
         }
