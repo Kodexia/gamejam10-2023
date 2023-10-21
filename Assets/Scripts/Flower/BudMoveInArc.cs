39ecf262f70c3e549d4f7a8c497a5461681bcc05
Fami6Xx <libojer@gmail.com>
2023-10-21 17:27:45 +0200

    fix: some bugs in spawning and flying of seed

--- Assets/Scripts/Flower/BudMoveInArc.cs
+++ Assets/Scripts/Flower/BudMoveInArc.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,38 +6,42 @@ public class BudMoveInArc : MonoBehaviour
 {
     public float duration = 1.0f; // Duration of the movement
     public float heightOffset = 5.0f; // How high the arc should be
-    private GameObject _bud;
-    private GameObject _flower;
 
-    public void ShootOutBud(Vector2 startingPosition, Vector2 endPosition, GameObject bud, GameObject flower)
+    public void ShootOutBud(Vector2 startingPosition, Vector2 endPosition, GameObject seed, GameObject flower)
     {
-        Debug.Log("Shoot out bud v2 - MoveInArc");
-        StartCoroutine(MoveObjectInArc(startingPosition, endPosition));
-        _bud = bud;
-        _flower = flower;
+        GameObject budInstance = Instantiate(seed, startingPosition, Quaternion.identity);
+        StartCoroutine(MoveObjectInArc(budInstance, flower, startingPosition, endPosition));
     }
 
-    private IEnumerator MoveObjectInArc(Vector2 startingPosition, Vector2 endPosition)
+    private IEnumerator MoveObjectInArc(GameObject budInstance, GameObject flowerInstance, Vector2 startingPosition, Vector2 endPosition)
     {
         float elapsedTime = 0;
         Vector2 vertex = (startingPosition + endPosition) / 2;
         vertex.y += heightOffset;
+        
+        Vector2 lastPosition = startingPosition;
 
         while (elapsedTime < duration)
         {
             elapsedTime += Time.deltaTime;
             float t = elapsedTime / duration;
-
-            // Lerp between start and vertex, and vertex and end based on t
+            
             Vector2 firstLerp = Vector2.Lerp(startingPosition, vertex, t);
             Vector2 secondLerp = Vector2.Lerp(vertex, endPosition, t);
+
+            Vector2 currentPosition = Vector2.Lerp(firstLerp, secondLerp, t);
+            budInstance.transform.position = currentPosition;
+
+            Vector2 deltaPosition = currentPosition - lastPosition;
+            float angle = Mathf.Atan2(deltaPosition.y, deltaPosition.x) * Mathf.Rad2Deg;
+            budInstance.transform.rotation = Quaternion.Euler(0, 0, angle);
             
-            // Lerp between the above two Lerp results to get the position on the arc
-            _bud.transform.position = Vector2.Lerp(firstLerp, secondLerp, t);
+            lastPosition = currentPosition;
 
             yield return null;
         }
 
-        GameObject newBudObject = Instantiate(_flower, endPosition, Quaternion.identity);
+        GameObject newBudObject = Instantiate(flowerInstance, endPosition, Quaternion.identity);
+        Destroy(budInstance);
     }
 }
