aa050e1d3a949e4cbc2de96259375c1179f77d1c
Párek8 <polakpetr8878@gmail.com>
2023-10-23 10:34:39 +0200

    newest build

--- Assets/Scripts/Player/CharacterMovementScript.cs
+++ Assets/Scripts/Player/CharacterMovementScript.cs
@@ -1,9 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Runtime.CompilerServices;
-using UnityEditor.SceneTemplate;
 using UnityEngine;
 
 public class CharacterMovementScript : MonoBehaviour
