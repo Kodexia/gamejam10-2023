6e5119e447d07a35411a187acbe4cac0a0179d93
Parek8 <polakpetr8878@gmail.com>
2023-10-23 18:26:45 +0200

    fix: seamless background variables

--- Assets/Scripts/Grass/SeamlessBackground.cs
+++ Assets/Scripts/Grass/SeamlessBackground.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 public class SeamlessBackground : MonoBehaviour
 {
     [field: SerializeField] RawImage image;
-    [field: SerializeField] float acceleration = 1f;
+    [field: SerializeField] float xAcceleration = -0.18f;
+    [field: SerializeField] float yAcceleration = -0.17f;
 
     CharacterMovementScript _playerMovement;
     private void Start()
@@ -15,6 +16,7 @@ public class SeamlessBackground : MonoBehaviour
     }
     void Update()
     {
+        Vector2 acceleration = new Vector2(xAcceleration, yAcceleration);
         image.uvRect = new Rect(image.uvRect.position + (Vector2)Vector3.Normalize(_playerMovement.MoveDirection) * Time.deltaTime * acceleration, image.uvRect.size);
     }
 }
